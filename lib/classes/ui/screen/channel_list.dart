e06ae747300fc8e87214983494c851b0c8d2aedf
Zier Erik <erik@halcyonmobile.com>
2020-02-02 13:51:16 +0200

    Add pull-to-search to Channel List screen

--- lib/classes/ui/screen/channel_list.dart
+++ lib/classes/ui/screen/channel_list.dart
@@ -9,21 +9,81 @@ class ChannelsScreen extends StatefulWidget {
 }
 
 class _ChannelsScreenState extends State<ChannelsScreen> {
-  final _channelList = Get.channelList();
+  final _channelListFuture = Get.channelList();
+  List<Channel> _fullChannelList;
+  List<Channel> _channelList;
+
+  FocusNode _searchFieldFocusNode;
+  ScrollController _scrollController;
+
+  var _isSearching = false;
 
   @override
   void initState() {
     super.initState();
+
+    _searchFieldFocusNode = FocusNode();
+
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (!_isSearching && _scrollController.offset < -100) {
+        setState(() {
+          _isSearching = !_isSearching;
+        });
+        _searchFieldFocusNode.requestFocus();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchFieldFocusNode.dispose();
+    _scrollController.dispose();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Channels"),
+        title: AnimatedCrossFade(
+          firstChild: TextField(
+            decoration: InputDecoration(hintText: "Search..."),
+            focusNode: _searchFieldFocusNode,
+            onChanged: (string) {
+              setState(() {
+                _channelList = _fullChannelList.where((channel) {
+                  return channel.name.contains(string);
+                });
+              });
+            },
+          ),
+          secondChild: Text("Channels"),
+          crossFadeState: _isSearching
+              ? CrossFadeState.showFirst
+              : CrossFadeState.showSecond,
+          duration: Duration(milliseconds: 100),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(_isSearching ? Icons.close : Icons.search),
+            onPressed: () {
+              setState(() {
+                _isSearching = !_isSearching;
+              });
+
+              if (_isSearching) {
+                _searchFieldFocusNode.requestFocus();
+              } else {
+                _searchFieldFocusNode.unfocus();
+              }
+            },
+          )
+        ],
       ),
-      body: FutureBuilder(
-        future: _channelList,
+      body: FutureBuilder<List<Channel>>(
+        future: _channelListFuture,
         builder: ((context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.none:
