9ebffe38b1103f21a60e55b462d628947e54c50c
Zier Erik <erik.interwebz@gmail.com>
2020-05-08 12:46:27 +0300

    Show loading indicator when loading lists

--- lib/screens/content/favorite_list_screen.dart
+++ lib/screens/content/favorite_list_screen.dart
@@ -22,10 +22,11 @@ class _FavoriteChannelListScreenState extends State<FavoriteChannelListScreen> {
         builder: ((context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.none:
-              return Text('Press button to start.');
             case ConnectionState.active:
             case ConnectionState.waiting:
-              return Text('Awaiting result...');
+              return Center(
+                child: CircularProgressIndicator(),
+              );
             case ConnectionState.done:
               if (snapshot.hasError) return Text('Error: ${snapshot.error}');
               return ListView.builder(
